e48c698ce11df6b919be7b9adab797b06678df78
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2019-12-23 14:21:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SDVX-Setup
+ 
---
+++ SDVX-Setup/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDVX_Setup
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern Boolean EnumDisplaySettings(
+            [param: MarshalAs(UnmanagedType.LPTStr)]
+            string lpszDeviceName,
+            [param: MarshalAs(UnmanagedType.U4)]
+            int iModeNum,
+            [In, Out]
+            ref DEVMODE lpDevMode);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmSpecVersion;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmDriverVersion;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmSize;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmDriverExtra;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmFields;
+
+            public POINTL dmPosition;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayOrientation;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFixedOutput;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmColor;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmDuplex;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmYResolution;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmTTOption;
+
+            [MarshalAs(UnmanagedType.I2)]
+            public Int16 dmCollate;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+
+            [MarshalAs(UnmanagedType.U2)]
+            public UInt16 dmLogPixels;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmBitsPerPel;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPelsWidth;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPelsHeight;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFlags;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDisplayFrequency;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmICMMethod;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmICMIntent;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmMediaType;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmDitherType;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmReserved1;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmReserved2;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPanningWidth;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dmPanningHeight;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINTL
+        {
+            [MarshalAs(UnmanagedType.I4)]
+            public int x;
+            [MarshalAs(UnmanagedType.I4)]
+            public int y;
+        }
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern int ChangeDisplaySettings(
+            [In, Out]
+            ref DEVMODE lpDevMode,
+            [param: MarshalAs(UnmanagedType.U4)]
+            uint dwflags);
+
+        public static void ChangeDisplaySettings (int width, int height, int frequency, int orientation)
+        {
+            DEVMODE originalMode = new DEVMODE();
+            originalMode.dmSize =
+                (ushort)Marshal.SizeOf(originalMode);
+
+            // Retrieving current settings
+            // to edit them
+            EnumDisplaySettings(null,
+                -1,
+                ref originalMode);
+
+            // Making a copy of the current settings
+            // to allow reseting to the original mode
+            DEVMODE newMode = originalMode;
+
+            // Changing the settings
+            newMode.dmPelsWidth = (uint)width;
+            newMode.dmPelsHeight = (uint)height;
+            newMode.dmDisplayFrequency = (uint)frequency;
+            newMode.dmDisplayOrientation = (uint)orientation;
+
+            // Capturing the operation result
+            int result = ChangeDisplaySettings(ref newMode, 0);
+
+            if (result == 0)
+                Console.WriteLine("Succeeded.");
+            else if (result == -2)
+                Console.WriteLine("Mode not supported.");
+            else if (result == 1)
+                Console.WriteLine("Restart required.");
+            else
+                Console.WriteLine("Failed. Error code = {0}", result);
+        }
+
+        static void Main(string[] args)
+        {
+            ChangeDisplaySettings(720, 1280, 60, 1);
+
+            ProcessStartInfo PSI = new ProcessStartInfo();
+            PSI.FileName = @"D:\Games\Bemani\Sound Voltex\HEAVENLY HAVEN\contents\spicecfg.exe";
+            using (Process proc = Process.Start(PSI))
+            {
+                proc.WaitForExit();
+            }
+
+            PSI.FileName = @"D:\Games\Bemani\Sound Voltex\HEAVENLY HAVEN\contents\xrpcv.exe";
+            Process.Start(PSI);
+
+            PSI.FileName = @"D:\Games\Bemani\Sound Voltex\HEAVENLY HAVEN\contents\spice.exe";
+            using (Process proc = Process.Start(PSI))
+            {
+                proc.WaitForExit();
+            }
+
+            ChangeDisplaySettings(1920, 1080, 144, 0);
+        }
+    }
+}
